4a44083da9f15687deb26bed84dcd4a933fb9a5b
pplprof <pplprof@PUCMINAS.NET>
2019-04-04 22:02:52 -0300

    Adicionar arquivos de projeto.

---
+++ LivrariaApi/Controllers/LivrariaController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LivrariaApi.Modelos;
+
+namespace LivrariaApi.Controllers
+{
+    [Route("livraria")]
+    [ApiController]
+    public class LivrariaController : ControllerBase
+    {
+        List<Livro> listaLivros;
+
+        public LivrariaController()
+        {
+            CriaTabelas();
+        }
+        
+        public void CriaTabelas()
+        {
+            listaLivros = new List<Livro>() {
+                new Livro { Id = 1, Nome = "Livro 1", Descricao = "Livro 1", Preco = 10, QuantPaginas = 10, CodCategoria = 1 },
+                new Livro { Id = 2, Nome = "Livro 2", Descricao = "Livro 2", Preco = 20, QuantPaginas = 40, CodCategoria = 2 },
+                new Livro { Id = 3, Nome = "Livro 3", Descricao = "Livro 3", Preco = 25, QuantPaginas = 350, CodCategoria = 2 }
+            };
+
+        }
+
+        [HttpGet("livros")]
+        public async Task<ActionResult<IEnumerable<Livro>>> GetLivros()
+        {
+            return listaLivros;
+        }
+        
+        [HttpGet("livros/{id}")]
+        public async Task<ActionResult<Livro>> GetLivros(int id)
+        {
+            var livro = listaLivros.Where(l=>l.Id == id).FirstOrDefault();
+
+            if (livro == null)
+            {
+                return NotFound();
+            }
+
+            return livro;
+        }
+
+        [HttpGet("livros/categoria/{id}")]
+        public async Task<ActionResult<List<Livro>>> GetLivrosPorCategoria(int id)
+        {
+            List<Livro> livros = listaLivros.Where(l => l.CodCategoria == id).ToList();
+
+            if (livros.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return livros;
+        }
+        
+        [HttpPost("livros")]
+        public async Task<ActionResult<List<Livro>>> CadastrarLivro(Livro livro)
+        {
+            Livro novoLivro = new Livro() { Id = (listaLivros.Max(l=>l.Id) + 1), Nome = livro.Nome, Descricao = livro.Descricao, CodCategoria = livro.CodCategoria, Preco = livro.Preco, QuantPaginas = livro.QuantPaginas };
+            listaLivros.Add(novoLivro);
+
+            return listaLivros;
+        }
+    }
+}
---
+++ LivrariaApi/Modelos/ItemPedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LivrariaApi.Modelos
+{
+    public class ItemPedido
+    {
+        public int Id { get; set; }
+        public int IdCarrinhoCompras { get; set; }
+        public int IdLivro { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
